6fce0dbd532d68032cd48f053ef35a7f422ebf9d
ezemart <ernesto.ops@gmail.com>
2024-06-03 22:24:13 -0300

    Configurar la aplicación para usar el puerto asignado por Heroku

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,25 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
 @SpringBootApplication
 public class DemoApplication {
+
+	@Value("${PORT:8080}")
+	private int port;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+		factory.setPort(port);
+		return factory;
+	}
 }
